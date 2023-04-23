a73dfb8eb5ad8861cf8c4fcc2e30695932b060ab
Christopher Moore <christopher.e.moore@gmail.com>
2023-04-23 18:58:10 +0800

    more

--- README.md
+++ README.md
@@ -10,12 +10,13 @@ That's for pictures in the last 12 hours.
 
 Then it seems data gets shifted to the archives.
 
-`https://www.ngdc.noaa.gov/stp/drap/data/2023/04/SWX_DRAP20_C_SWPC_20230422.tar.gz`
+`https://www.ngdc.noaa.gov/stp/drap/data/2023/04/SWX_DRAP20_C_SWPC_20230422.tar.gz` where files are stored at root-level. 
 
 But you can't get archived data within the last 3 days.
 
-Then the next two days worth or so are .zips.  Which are big.
+Then the next two days worth or so are .zips.  Which are 2x as big (less-compressed) as the .tar.gz, but contain identical info, except that the .zip stores its folders in an 'archive-something' subfolder.
 
-Then from there on back everything is .tar.gz's.
+Then from that point in time on back everything is .tar.gz's.
 
-The tgz and zips contain the same files.
+
+For a given folder, the .tar.gz is about 70 mb, the .zip is about 120 mb ... and me recompressing as .7z is 45 mb ... and me recompressing as .zip is 20mb ... How come NOAA's own .zips are 10x as big as mine?
--- run.lua
+++ run.lua
@@ -1,26 +1,37 @@
 #!/usr/bin/env luajit
 local file = require 'ext.file'
 local table = require 'ext.table'
+local Zip = require 'zip'
 
 local startTS, endTS = ...
 assert(startTS and endTS, [[
 expected (startTS) (endTS)
-timestamps are in the format yyyymmddHHMM and are in UTC
+timestamps are in the format yyyy/mm/dd/HH:MM:SS and are in UTC
 seconds are ignored/omitted.
+I don't care what separators you use in the timestamps.
+Also, seconds are thrown away. Resolution is to the minute.
 ]])
 
 
-local function parseTime(s)
+local function parseTimestamp(s)
+	-- I'm not using match because i want optional args
 	return os.time{
 		year = tonumber(s:sub(1,4)),
-		month = tonumber(s:sub(5,6)),
-		day = tonumber(s:sub(7,8)),
-		hour = tonumber(s:sub(9,10)),
-		min = tonumber(s:sub(11,12)),
-		sec = tonumber(s:sub(13,14)),
+		month = tonumber(s:sub(6,7)),
+		day = tonumber(s:sub(9,10)),
+		hour = tonumber(s:sub(12,13)),
+		min = tonumber(s:sub(15,16)),
+		sec = tonumber(s:sub(18,17)),
 	}
 end
 
+local function roundMin(t)
+	local d = os.date('*t', t)
+	d.sec = 0
+	return os.time(d)
+end
+
+-- TODO how about a day-iter between timestamps?
 local function roundDay(t)
 	local d = os.date('*t', t)
 	d.hour = 0
@@ -43,49 +54,100 @@ local function exec(s)
 	return printAndReturn(os.execute(s))
 end
 
-local startTime = parseTime(startTS)
-print('startTime', require 'ext.tolua'(startTime))
-local endTime = parseTime(endTS)
-print('endTime', require 'ext.tolua'(endTime))
+local startMin = roundMin(parseTimestamp(startTS))
+print('startMin', os.date(nil, startMin))
+local endMin = roundMin(parseTimestamp(endTS))
+print('endMin', os.date(nil, endMin))
 
-local startDay = roundDay(startTime)
-local endDay = roundDay(endTime)
+local startDay = roundDay(startMin)
+local endDay = roundDay(endMin)
 
-file'cache':mkdir()
+local cachedir = 'cache'
+-- assert(file(cachedir):mkdir()) ?
+file(cachedir):mkdir()
+assert(file(cachedir):isdir())
 
 local function tryToDownload(url)
 	local fn = url:match('[^/]*$')
 	--print(url, fn)
-	local cacheFn = 'cache/'..fn
+	local cacheFn = cachedir..'/'..fn
 	if not file(cacheFn):exists() then
-		exec('cd cache && wget '..url)
+		exec('cd "'..cachedir..'" && wget '..url)
 	end
 	return cacheFn
+end
+
+-- TODO download either, then extract-and-recompress it ... in cachedir/%Y%m%d.zip or .something
+local function download(t)
+	error("still gotta do unzipping")
+	local urlWithoutExt = urlWithoutExtForTime(t)
+	local fn = tryToDownload(urlWithoutExt..'.zip')
+	if fn then
+		-- TODO convert .zip to .7z?  or extract both?
+		return fn
+	else
+		fn = tryToDownload(urlWithoutExt..'.tar.gz')
+		if fn then
+			return fn
+		else
+			error("couldn't find")
+		end
+	end
+end
+
+-- TODO instead of a table, just save the last one, since we are iterating through in order
+local zipArchivesForFileName = {}
+local function getZipArchive(zipFileName)
+	local zipArchive = zipArchivesForFileName[zipFileName]
+	if zipArchive then return zipArchive end
+	if not file(zipFileName):exists() then
+		download(t)
+		assert(file(zipFileName):exists())
+	end
+	zipArchive = Zip(zipFileName)
+	zipArchivesForFileName[zipFileName] = zipArchive
+	return zipArchive
+end
+
+file'tmp':mkdir()
+assert(file'tmp':isdir())
+for f in file'tmp':dir() do
+	file('tmp/'..f):remove()
 end
 
 -- `https://services.swpc.noaa.gov/images/animations/d-rap/global/d-rap/SWX_DRAP20_C_SWPC_20230422142400_GLOBAL.png`
 local fs = table()
 -- this won't skip days ... right ... ?  rounding error?  weird time standards?  leap seconds? idk?
 -- ig a better way to do it is inc the timestep by a day and a half then round it down to the nearest day's timestamp ...
-for t=startDay,endDay,60*60*24 do
-	local urlWithoutExt = urlWithoutExtForTime(t)
-	local fn = tryToDownload(urlWithoutExt..'.zip')
-	if fn then
-		-- TODO convert .zip to .7z?  or extract both?
-		fs:insert(fn)
+local failCount = 0
+local count = 0
+for t=startMin,endMin,60 do
+	count = count + 1
+	local zipFileName = cachedir..'/'..os.date('%Y%m%d', t)..'.zip'
+	local zipArchive = getZipArchive(zipFileName)
+	local fileNameInArchive = os.date('SWX_DRAP20_C_SWPC_%Y%m%d%H%M00_GLOBAL.png', t)
+	local zipPath = zipArchive:file(fileNameInArchive)
+print('zipArchive', zipArchive)	
+print('zipPath', zipPath)	
+	if not zipPath:exists() then
+		print("failed to find filename "..fileNameInArchive)
+		failCount = failCount + 1
+		fs:insert(fs:last())	-- insert last frame anyways if it's there so there's no skips
 	else
-		fn = tryToDownload(urlWithoutExt..'.tar.gz')
-		if fn then
-			fs:insert(fn)
-		else
-			error("couldn't find")
-		end
+		-- extract to tmp
+		local dstfn = count..'.png' 
+		file('tmp/'..dstfn):write((zipPath:read()))
+		fs:insert(dstfn)	-- relative to the tmp dir
 	end
 end
-fs:sort()
---[[
--- also in my obs-buildvideo project
--- mabye merge if it wasn't so simple
-file'input.txt':write(fs:mapi(function(s) return "file '"..s..'"' end):concat'\n'..'\n')
-exec('ffmpeg -y -i input.txt out.mp4')
---]]
+print('found '..(count - failCount)..' of '..count..' files')
+if #fs == 0 then error("can't go any further") end
+file'tmp/input.txt':write(fs:mapi(function(s,i)
+	return "file '"..s.."'\n"
+		..(i < #fs and 'duration 1\n' or '')
+end):append{
+	#fs > 0 and ("file '"..fs:last().."'\n") or nil
+}:concat())
+exec('ffmpeg -r 24 -y -f concat -i tmp/input.txt out.mp4')
+-- ok at 24 fps , 1 frame per second, 1 day becomes 1 minute
+-- and our 1 min vid is about 1.7 mb
